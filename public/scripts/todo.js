9325b80f62c5fc05b3db801c314186c3d82d0532
simonhn <simonhn@gmail.com>
2015-11-04 11:48:53 +0100

    now fully functional

--- public/scripts/todo.js
+++ public/scripts/todo.js
@@ -1,4 +1,23 @@
 var ToDoBox = React.createClass({
+  delc: function(data_id) {
+    console.log(data_id);
+    var toDoItems = this.state.data;
+    var newToDoItems = toDoItems.filter(function(elem) {
+      return elem.id != data_id;
+    });
+    
+    this.setState({data: newToDoItems});
+    $.ajax({
+      type: 'DELETE',
+      url: '/api/todos/' + data_id + '/',
+      success: function() {
+          //...
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
   handleToDoSubmit: function(toDoItem) {
     
     //update todo list before actually sending to server :)
@@ -39,7 +58,8 @@ var ToDoBox = React.createClass({
   render: function() {
     return (
       <div className="toDoBox">
-        <ToDoList data={this.state.data} />
+        <h1>ToDo listen over alle todo lister</h1>
+        <ToDoList del={this.delc} data={this.state.data} />
         <ToDoForm onToDoSubmit={this.handleToDoSubmit} />
       </div>
     );
@@ -47,26 +67,36 @@ var ToDoBox = React.createClass({
 });
 
 var ToDoItem = React.createClass({
+  handleClick: function(e){
+    console.log(this.props);
+    e.preventDefault();
+    var toDoItemId = this.props.todoitem.id;
+    return this.props.onDelete(toDoItemId);
+  },
   render: function() {
     return (
       <div className="toDoItem">
         <div className="toDoName">
-          {this.props.name}
+          {this.props.todoitem.name}
         </div>
-        <button className="delete"></button>
+        <button onClick={this.handleClick} className="delete"></button>
       </div>
     );
   }
 });
 
 var ToDoList = React.createClass({
+  handleDelete: function(toDoItemId){
+    return this.props.del(toDoItemId);
+  },
   render: function() {
+    var onDelete = this.props.onDelete;
     var todoNodes = this.props.data.map(function (todoItem) {
       return (
-        <ToDoItem key={(+new Date() + Math.floor(Math.random() * 99))} name={todoItem.name}>
+        <ToDoItem onDelete={this.handleDelete} key={(+new Date() + Math.floor(Math.random() * 99))} todoitem={todoItem}>
         </ToDoItem>
       );
-    });
+    }, this);
     return (
       <div className="toDoList">
         {todoNodes}
@@ -90,8 +120,8 @@ var ToDoForm = React.createClass({
   render: function() {
     return (
       <form className="toDoForm" onSubmit={this.handleSubmit}>
-        <input type="text" placeholder="Add todo item" ref="name" />
-        <input type="submit" value="Post" />
+        <input type="text" placeholder="Husk" ref="name" />
+        <input type="submit" value="Tilføj" />
       </form>
     );
   }
